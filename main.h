cb5f5d68c371b03eefac81e6fd00d042fdd286f7
lawaljubril <lawalj75@gmail.com>
2022-11-23 13:22:20 -0800

    Education is when you read the fine print. Experience is what you get if you don't

--- main.h
+++ main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stddef.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 int _putchar(char c);
 int _printf(const char *format, ...);
